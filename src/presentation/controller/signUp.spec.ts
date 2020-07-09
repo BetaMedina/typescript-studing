74e8ca35a39fad81b57751d31e3154c80c3d8bff
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-07-09 15:49:47 -0300

    Inserindo testes estilo TDD

---
+++ src/presentation/controller/signUp.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signUpController'
+
+describe('Sign up Controller', () => {
+  it('Should return error if no name is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    await expect(httpResponse.statusCode).toBe(400)
+  })
+  // it('Should pass parameters and create user account', () => {
+
+  // })
+})
