9b8684e1a53c436b0afe5bb434fd9021a5e2d229
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-07-17 18:11:05 -0300

    Veirificando se o cors esta ativo

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { expressJson } from '../middlewares/express-json'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express):void => {
   app.use(expressJson)
+  app.use(cors)
 }
