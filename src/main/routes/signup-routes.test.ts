aacc7560003c074100959dc9695e1c662d7dfc16
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-07-18 11:56:54 -0300

    conectando no mongoDb utilizando os testes

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,21 @@
-import { Request, Response } from 'express'
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo.helper'
 
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
