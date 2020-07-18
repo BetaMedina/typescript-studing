20edea2d93ad09b7226f8a3625e6d0d5a6e5c290
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-07-18 11:09:23 -0300

    Trocando o res-type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { expressJson } from '../middlewares/express-json'
 import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/content-type'
 
 export default (app: Express):void => {
   app.use(expressJson)
   app.use(cors)
+  app.use(contentType)
 }
