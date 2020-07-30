56c39e4bdf7ef014fb84fdac1cc717f224367ff2
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-07-30 19:51:52 -0300

    Adaptando os factorys para usarem os decorators

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,11 +3,15 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/addAccount/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountRepository } from '../../infra/db/mongodb/account-repository/account'
+import { ControllerInterface } from '../../presentation/protocols'
+import { LogErrorControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = ():SignUpController => {
+export const makeSignUpController = ():ControllerInterface => {
   const accountRepository = new AccountRepository()
   const bcryptAdapter = new BcryptAdapter(12)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  return new LogErrorControllerDecorator(signUController)
 }
